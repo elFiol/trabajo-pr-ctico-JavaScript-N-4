0d7bf3efac2fec1adeee7c476a25081bd2b8d7c7
Fiol Thiago <fiolthiago9@gmail.com>
2023-11-18 18:08:46 -0300

    agregamos el punto uno y agregamos estructura del proyecto

---
+++ js/p-1.js
@@ -0,0 +1,13 @@
+const auto = {
+  color: "rojo",
+  marca: "Ford",
+  modelo: "Clase F",
+  encender: function () {
+    document.write("<p>el auto esta encendido</p>");
+  },
+  apagar: function () {
+    document.write("<p>el auto esta apagado</p>");
+  },
+};
+auto.encender();
+auto.apagar();
